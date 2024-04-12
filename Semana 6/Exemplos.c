16e47ceb9ac11389af48db0db84663eb7ef97a63
Artprozew <artprozew@gmail.com>
2024-04-11 23:05:08 -0300

    Organizado exemplos da Semana 6

--- Semana 6/Exemplos.c
+++ Semana 6/Exemplos.c
@@ -1,70 +1,58 @@
 #include <stdio.h>
-
-int main (void) {
-
-    int vetor[3] = {1, 2, 3};
-    int *ponteiro;
-
-    ponteiro = vetor;
-    printf("Endereco vetor e indice 0: %d\n", *ponteiro);
-    ponteiro++;
-    printf("Endereco indice 1: %d\n", *ponteiro);
-    ponteiro++;
-    printf("Endereco indice 2: %d\n", *ponteiro);
-    ponteiro++;
-    printf("Endereco 4bytes depois: %d\n", *ponteiro);
-
-    /*
-        for (int i = 0; i < 3; i++) {
-            ponteiro = &vetor[i];
-
-            printf("Indice %d: %p\n", i, ponteiro);
-        } 
-    */   
-
-    return 0;
-}
-
-
-/*
-
-#include <stdio.h>
-
-int main (void) {
-
-    int x, *p;
-    x = 10;
-    p = &x;
-
-    printf ("Valor armazenado em P: %d\n", p);
-    printf ("Endereco de X: %d\n", &x);
-    printf ("Endereco de P: %d\n", &p);
-    printf ("Valor armazenado em X: %d\n", x);
-    printf ("Valor armazenado no endereco de memoria em *P: %d\n", *p);
-
-    return 0;
-}
-
-=============================================================================
-#include <stdio.h>
+#include <locale.h>
 
 void minhaFuncao (int *a, int *b) {
     int resultado;
 
-    resultado = *a+*b;
+    resultado = *a + *b;
 
-    printf ("%d", resultado);
+    printf ("%d\n", resultado);
 }
 
 int main (void) {
+    setlocale(LC_ALL, "portuguese");
 
-    int x, y;
-
-    x = 10;
-    y = 20;
+    // Exemplo 1
+    printf("Exemplo 1:\n");
+    int A, B;
+    A = 10;
+    B = 20;
     
-    minhaFuncao(&x, &y);
+    minhaFuncao(&A, &B);
+
+    // Exemplo 2
+    int vetor[3] = {1, 2, 3};
+    int *ponteiro;
+
+    printf("\nExemplo 2:\n");
+    ponteiro = vetor;
+    printf("Endereço indice 0: %d\n", *ponteiro);
+    ponteiro++;
+    printf("Endereço indice 1: %d\n", *ponteiro);
+    ponteiro++;
+    printf("Endereço indice 2: %d\n", *ponteiro);
+    //ponteiro++;
+    //printf("Endereço 4bytes depois: %d\n", *ponteiro);
+
+    // Exemplo 3
+    int x, *p;
+    x = 10;
+    p = &x;
+
+    printf("\nExemplo 3:\n");
+    printf("Valor armazenado em p: %p\n", p); // Valor armazenado em P: Endereço de memória hexadecimal A
+    printf("Endereço de p: %p\n", &p); // Endereço de p > Endereço de memória hexadecimal B (endereço do ponteiro em si)
+    printf("Endereço de x: %p\n", &x); // Endereço de x > Endereço de memória hexadecimal A
+    printf("Valor armazenado em X: %d\n", x); // Valor armazenado em X: 10
+    printf("Valor armazenado no endereço de memória que está em p: %d\n", *p); // Valor armazenado no endereço de memória que está em p > X: 10
+
+    // Exemplo 4
+    printf("\nExemplo 4:\n");
+    for (int i = 0; i < 3; i++) {
+        ponteiro = &vetor[i];
+
+        printf("Indice %d: %p\n", i, ponteiro);
+    } 
 
     return 0;
 }
-*/
