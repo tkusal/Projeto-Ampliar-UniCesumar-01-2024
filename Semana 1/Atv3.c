7439bf252a27ee78778c2f8b53656675d816ee06
CallB0y <ryannicolau34@gmail.com>
2024-05-07 20:50:35 -0300

    Atividade 3 resolvida

--- Semana 1/Atv3.c
+++ Semana 1/Atv3.c
@@ -1,4 +1,31 @@
 /*
-    Escreva um programa que receba a idade de uma pessoa e informe se ela é maior de idade.
+    Escreva um programa que receba a idade de uma pessoa e informe se ela ï¿½ maior de idade.
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main(){
+    setlocale(LC_ALL, "portuguese");
+
+    int idade;
+
+    printf("insira sua idade: \n");
+    scanf("%d", &idade);
+
+    if (idade >= 18){
+        printf("maior de idade!!!\n");
+    }else {
+        printf("menor de idade!!!\n");
+    }
+
+
+
+
+
+return 0;
+
+
+
+}
