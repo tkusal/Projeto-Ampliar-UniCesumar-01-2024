dfe07e43b28e2e72fbea6a30eaf2e375fb9b86a9
Ryan Nicolau <122414787+CallB0y@users.noreply.github.com>
2024-05-07 20:09:02 -0300

    Atividade 2 resolvida  (#5)
    * main
    
    * Atividade 2 resolvida
    
    * Delete Semana 1/atv2 Ryan Nicolau.c
    
    * Aula 02/05/2024

--- Semana 1/Atv2.c
+++ Semana 1/Atv2.c
@@ -1,4 +1,22 @@
 /*
-    Escreva um programa em C que receba o salário de uma pessoa e informe quanto ele pagará de imposto de renda (alíquota: 15%)
+    Escreva um programa em C que receba o salï¿½rio de uma pessoa e informe quanto ele pagarï¿½ de imposto de renda (alï¿½quota: 15%)
 */
 
+#include <stdio.h>
+#include <locale.h>
+
+int main(){
+
+    setlocale(LC_ALL, "Portuguese");
+    float salario, imposto;
+
+    printf("Insira seu salario a ser calculado: \n");
+    scanf("%f", &salario);
+
+    imposto = salario * 0.15;
+
+    printf("O valor pago de imposto renda eh de %.2f!!!", imposto);
+
+
+   return 0;
+}
