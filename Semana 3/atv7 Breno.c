64ad0c27039679dd555e05ab9863eadc30478a18
BGUni77 <161780235+BGUni77@users.noreply.github.com>
2024-05-21 20:30:02 -0300

    Create atv7 Breno.c

---
+++ Semana 3/atv7 Breno.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main () {
+
+    setlocale(LC_ALL, "Portuguese");
+
+    int numero[10], aux;
+
+    for(int i = 0; i < 10; i++){
+        printf("Informe um número: ");
+        scanf("%d", &numero[i]);
+    }
+
+    for(int i = 0; i < 10; i++){
+        for (int j = 0; j < 10-i-1; j++) {
+            if(numero[j] > numero[j+1]) {
+            aux = numero[j];
+            numero[j] = numero[j+1];
+            numero[j+1] = aux;
+        }        
+    }
+    
+    
+    for (int i = 0; i < 10; i++){
+        printf(" %d ", numero[i]);
+    }
+    
+    return 0;
+}
+    
