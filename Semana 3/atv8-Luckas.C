650d054d8495b9b07f3844ed56754f7fba95e267
Luckas lima <168789392+Lucklyon@users.noreply.github.com>
2024-05-22 20:35:53 -0300

    Create atv8-Luckas.C (#17)

---
+++ Semana 3/atv8-Luckas.C
@@ -0,0 +1,28 @@
+/*
+    Crie um programa que receba números inteiros em uma matriz quadrada 3x3 e 
+    exiba na tela a soma dos números em sua diagonal 
+*/
+#include <stdio.h>
+
+int main(){
+
+    int num[3][3];
+
+    int aux1=0;
+
+    for(int i = 0; i < 3; i=i+1){
+        for(int j = 0; j < 3; j=j+1){
+            printf("Escreva o numero:");
+            scanf("%d", &num[i][j]);
+
+            if(i==j){
+                
+                aux1 += num[i][j];
+
+            }
+        }
+    }
+     
+     printf("%d", aux1);
+
+}
